bb7bd0dbdb8d649f874431cd1e3086f5d50a8b99
ByteBlitzer <securelycrazy@gmail.com>
2023-08-21 10:26:05 +1000

    Create 1-10answers.py

---
+++ challenge answers/1-10answers.py
@@ -0,0 +1,18 @@
+# print('Hello world')
+
+# name = "Cam"
+# for n in range(10):
+#     print(name)
+    
+user = []
+tot = 0
+for i in range(1):
+    user.insert(i, input('Enter Name: '))
+print(user)
+
+for n in range(1):
+    tot = 
+    
+
+
+
